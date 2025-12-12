17c9ce8389ceb5448e1e06bced5e1fae784f9346
Kishore Ganesh <kishore2912000@gmail.com>
2025-12-11 21:20:59 -0800

    Handle case when normals need to be inferred for triangle
    Signed-off-by: Kishore Ganesh <kishore2912000@gmail.com>

--- src/obj_parser.rs
+++ src/obj_parser.rs
@@ -1,5 +1,6 @@
 use glm::{make_vec3, TVec3};
 use obj::{load_obj, Obj, Vertex};
+use obj_parser_external::{ObjResult, Position};
 use crate::triangle::{NormalType, Triangle};
 use crate::triangle_mesh::TriangleMesh;
 use std::fs::File;
@@ -16,27 +17,44 @@ pub fn parse<P: AsRef<Path>>(_: TVec3<f32>, path: P) -> TriangleMesh{
     //f: vertex_index/texture_index/normal_index
     //Skip lines starting with #
     let input_buf_reader = BufReader::new(File::open(&path).unwrap());
-    let parsed_obj: Obj<Vertex, u16> = load_obj(input_buf_reader).unwrap();
-    let points: Vec<TVec3<f32>> =  parsed_obj.vertices.clone().into_iter().map(|x| make_vec3(&x.position)).collect();
-    let normals: Vec<TVec3<f32>> =  parsed_obj.vertices.clone().into_iter().map(|x| make_vec3(&x.normal)).collect();
-    let indices: Vec<u16> = parsed_obj.indices;
-
-    
-
+    let parsed_obj: ObjResult<Obj<Vertex, u16>> = load_obj(input_buf_reader);
     let mut triangles: Vec<Triangle> = Vec::new();
-    for index in 0..indices.len() {
-        if index%3==0 {
-            let triangle_points = [
-                points[indices[index] as usize],
-                points[indices[index+1] as usize],
-                points[indices[index+2] as usize]
-            ];
-            let triangle_normals = [
-                normals[indices[index] as usize],
-                normals[indices[index+1] as usize],
-                normals[indices[index+2] as usize]
-            ];
-            triangles.push(Triangle::create(triangle_points, NormalType::VertexNormals(triangle_normals)));
+    if let Ok(parsed_obj) = parsed_obj {
+        let points: Vec<TVec3<f32>> =  parsed_obj.vertices.clone().into_iter().map(|x| make_vec3(&x.position)).collect();
+        let normals: Vec<TVec3<f32>> =  parsed_obj.vertices.clone().into_iter().map(|x| make_vec3(&x.normal)).collect();
+        let indices: Vec<u16> = parsed_obj.indices;
+
+        for index in 0..indices.len() {
+            if index%3==0 {
+                let triangle_points = [
+                    points[indices[index] as usize],
+                    points[indices[index+1] as usize],
+                    points[indices[index+2] as usize]
+                ];
+                let triangle_normals = [
+                    normals[indices[index] as usize],
+                    normals[indices[index+1] as usize],
+                    normals[indices[index+2] as usize]
+                ];
+                triangles.push(Triangle::create(triangle_points, NormalType::VertexNormals(triangle_normals)));
+            }
+        }
+    }
+    else if let Err(_) = parsed_obj {
+        // The normals aren't present in the object file
+        let input_buf_reader = BufReader::new(File::open(&path).unwrap());
+        let parsed_obj: Obj<Position, u16> = load_obj(input_buf_reader).unwrap();
+        let points: Vec<TVec3<f32>> =  parsed_obj.vertices.clone().into_iter().map(|x| make_vec3(&x.position)).collect();
+        let indices = &parsed_obj.indices;
+        for index in 0..indices.len() {
+            if index%3==0 {
+                let triangle_points = [
+                    points[indices[index] as usize],
+                    points[indices[index+1] as usize],
+                    points[indices[index+2] as usize]
+                ];
+                triangles.push(Triangle::create(triangle_points, NormalType::Inferred))
+            }
         }
     }
     
